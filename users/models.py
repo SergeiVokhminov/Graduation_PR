a905bdab18c5ff0fc839e0546b1416b830ca4e1f
sergei_vokhminov <rreaper@list.ru>
2025-02-08 00:03:13 +0300

    выполнены миграции и модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Поля для модели пользователя."""
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Электронная почта")
+    phone_number = models.CharField(
+        max_length=35, verbose_name="Номер телефона", blank=True, null=True
+    )
+    city = models.CharField(
+        max_length=50,
+        verbose_name="Город",
+        help_text="Введите город",
+        blank=True,
+        null=True,
+    )
+    avatar = models.ImageField(
+        upload_to="photo/avatars/",
+        verbose_name="Аватар",
+        help_text="Загрузите Ваш аватар",
+        blank=True,
+        null=True,
+    )
+    tg_id = models.CharField(
+        max_length=300,
+        verbose_name="ID профиля Telegram",
+        help_text="Введите ID Вашего профиля Telegram",
+        blank=True,
+        null=True,
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.email}"
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    CreateAPIView,
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+)
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateApiView(CreateAPIView):
+    """Контроллер создания пользователя."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserListApiView(ListAPIView):
+    """Контроллер просмотра списка пользователей."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailApiView(RetrieveAPIView):
+    """Контроллер просмотра информации о пользователе."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserUpdateApiView(UpdateAPIView):
+    """Контроллер изменения пользователя."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDeleteApiView(DestroyAPIView):
+    """Контроллер удаления пользователя."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
