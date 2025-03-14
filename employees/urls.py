a0e861552839e4cb573df97580098a647f921b11
sergei_vokhminov <rreaper@list.ru>
2025-03-14 11:09:49 +0300

    корректировка приложения employees

--- employees/urls.py
+++ employees/urls.py
@@ -7,6 +7,7 @@ from employees.views import (
     EmployeeListAPIView,
     EmployeeRetrieveAPIView,
     EmployeeUpdateAPIView,
+    BusyEmployeesView,
 )
 
 app_name = EmployeesConfig.name
@@ -14,7 +15,12 @@ app_name = EmployeesConfig.name
 urlpatterns = [
     path("create/", EmployeeCreateAPIView.as_view(), name="employee_create"),
     path("list/", EmployeeListAPIView.as_view(), name="employee_list"),
-    path("detail<int:pk>/", EmployeeRetrieveAPIView.as_view(), name="employee_detail"),
+    path("detail/<int:pk>/", EmployeeRetrieveAPIView.as_view(), name="employee_detail"),
     path("update/<int:pk>/", EmployeeUpdateAPIView.as_view(), name="employee_update"),
     path("delete/<int:pk>/", EmployeeDestroyAPIView.as_view(), name="employee_delete"),
+    path(
+        "busy_employees/",
+        BusyEmployeesView.as_view({"get": "busy_employees"}),
+        name="busy_employees",
+    ),
 ]
